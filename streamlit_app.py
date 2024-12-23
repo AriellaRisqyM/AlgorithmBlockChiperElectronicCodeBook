6c37fbc2820f03544af362186660f0572eab432e
AriellaRisqyM <ariellarisqy@gmail.com>
2024-12-23 11:38:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,10 +60,12 @@ def ecb_decrypt(ciphertext, key):
     return plaintext.rstrip('0'), plaintext_hex, process
 
 # Streamlit Interface
+st.title("Dibuat oleh ARIELLA RISQY MAULANA - A11.2022.14035 & BIMA NUR ABDILLAH - A11.2022.14041")
+st.write("TUGAS KRIPTOGRAFI A11.4509")
+
 st.title("🎈 Enkripsi dan Dekripsi ECB")
 st.write("Pilih opsi untuk melakukan enkripsi atau dekripsi dan masukkan plaintext serta key.")
-st.title("Dibuat oleh ARIELLA RISQY MAULANA - A11.2022.14035 & BIMA NUR ABDILLAH -A11.2022.14041")
-st.write("TUGAS KRIPTOGRAFI A11.4509")
+
 
 # Tata letak kolom
 col1, col2, col3 = st.columns(3)
